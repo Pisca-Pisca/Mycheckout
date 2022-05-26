912bbb29908d85c8fd459c8310311375e65f873e
Nickcarv18 <nicolicarvalho1@hotmail.com>
2022-05-26 09:42:19 -0300

    Correção enum forma de pagamento

--- src/Enum/FormasPagamento.java
+++ src/Enum/FormasPagamento.java
@@ -1,7 +1,17 @@
 package Enum;
 
 public enum FormasPagamento {
-    PIX,
-    CARTÃO,
-    DINHEIRO
+    PIX("Pix"),
+    CARTAO("Cartão"),
+    DINHEIRO("Dinheiro");
+    
+    private final String pagamento;
+    
+    FormasPagamento(String pagamento) {
+        this.pagamento = pagamento;
+    }
+
+    public String getPagamento() {
+        return pagamento;
+    }
 }
